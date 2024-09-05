b18ece7fc7d2ccde4440029548070400a202e505
felixkur1 <fk494154@gmail.com>
2024-09-05 21:21:18 +0700

    menambah fungsi keliling lingkaran

--- app.py
+++ app.py
@@ -6,3 +6,11 @@ def luas_persegi_panjang():
   print("Luas persegi adalah: ", luas)
 
 luas_persegi_panjang()
+
+def keliling_persegi_panjang():
+  p =  int(input("Masukkan panjang persegi: "))
+  l =  int(input("Masukkan lebar persegi: "))
+  keliling = 2 * (p + l)
+  print("Keliling persegi adalah: ", keliling)
+
+keliling_persegi_panjang()
