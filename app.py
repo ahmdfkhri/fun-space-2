7aa1152902117eb44eef476e5d5398f163141bdc
ahmdfkhri <fakhri.ahmad030304@gmail.com>
2024-09-05 20:28:41 +0700

    Fungsi luas persegi panjang

---
+++ app.py
@@ -0,0 +1,8 @@
+# Hitung luas Persegi Panjang
+def luas_persegi_panjang():
+  p =  int(input("Masukkan panjang persegi: "))
+  l =  int(input("Masukkan lebar persegi: "))
+  luas = p * l
+  print("Luas persegi adalah: ", luas)
+
+luas_persegi_panjang()
