ea79eb01a1735d9b5a9d400e2f0c1b81b81c380e
felixkur1 <fk494154@gmail.com>
2024-09-05 22:01:33 +0700

    tambahan upgrade

---
+++ tambahan.py
@@ -0,0 +1,7 @@
+def hitung_luas_segitiga():
+    alas =  int(input("Masukkan alas segitiga: "))
+    tinggi =  int(input("Masukkan tinggi segitiga: "))
+    luas = 0.5 * alas * tinggi
+    print("Luas segitiga =", luas)
+
+hitung_luas_segitiga()
